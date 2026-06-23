30c71c3d4fa80f1c2dfd0a8b42c7f5449827d1eb
RetiredHunter <belhassinehedi308@gmail.com>
2026-06-23 17:58:59 -0400

    #4 - TaskSection in progress

--- Frontend/src/HomePage.tsx
+++ Frontend/src/HomePage.tsx
@@ -1,37 +1,52 @@
 export default function HomePage() {
     return <>
-        <NavBar />
-        <SubNavBar />
-        <TaskView />
+        <Nav />
+        <SubNav />
+        <main className="tasks-view">
+            <TaskSection header="Dailies" type="repeating-tasks-section" />
+            <TaskSection header="To Dos" type="normal-tasks-section" />
+            <TaskSection header="Pending" type="pending-tasks-section" />
+        </main>
     </>
 }
 
-function NavBar() {
-    return <nav>
-        <ul>
-            <li><a href="#" title="Logo">Logo</a></li>
-            <li><a href="#" title="Tasks">Tasks</a></li>
-            <li><a href="#" title="Party">Party</a></li>
-            <li><a href="#" title="Stats">Stats</a></li>
-            <li><a href="#" title="Shop">Shop</a></li>
-            <li><a href="#" title="About">About</a></li>
-            <div className='navend'>
-                <li><a href="#" title="Gems">G1</a></li>
-                <li><a href="#" title="Coins">C327</a></li>
-                <li><a href="#" title="Profile">Logo</a></li>
-            </div>
-        </ul>
-    </nav>
+function Nav() {
+    return <header>
+        <nav>
+            <ul>
+                <li><a href="#" title="Logo">Logo</a></li>
+                <li><a href="#" title="Tasks">Tasks</a></li>
+                <li><a href="#" title="Party">Party</a></li>
+                <li><a href="#" title="Stats">Stats</a></li>
+                <li><a href="#" title="Shop">Shop</a></li>
+                <li><a href="#" title="About">About</a></li>
+                <div className='nav-end'>
+                    <li><a href="#" title="Gems">G1</a></li>
+                    <li><a href="#" title="Coins">C327</a></li>
+                    <li><a href="#" title="Profile">Logo</a></li>
+                </div>
+            </ul>
+        </nav>
+    </header>
 }
 
-function SubNavBar() {
-    return <div className="subnavbar">
+function SubNav() {
+    return <div className="sub-nav">
         <h1>LV 4</h1>
     </div>
 }
 
-function TaskView() {
-    return <>
+function TaskSection({ header, type }) {
+    return <section className={`tasks-section ${type}`}>
+        <h3>{header}</h3>
+        <div className="tasks-list">
+            <Task />
+        </div>
+    </section>
+}
 
-    </>
+function Task(){
+    return <div className="task">
+        
+    </div>
 }
