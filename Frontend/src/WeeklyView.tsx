c91a64b49189c86e5321ed27437eb6bc5b4e59c0
Yaman Alhamamy <yamanmh2002@gmail.com>
2026-06-25 18:48:40 +0300

    #9 Add: WeeklyView/ViewButton func

---
+++ Frontend/src/WeeklyView.tsx
@@ -0,0 +1,18 @@
+import {Nav,SubNav} from "./HomePage.tsx";
+
+export default function WeeklyView() {
+    return <>
+        <Nav/>
+        <SubNav/>
+        <ViewButton/>
+
+    </>;
+}
+
+function ViewButton() {
+    return <div>
+        <select>
+
+        </select>
+    </div>
+}
