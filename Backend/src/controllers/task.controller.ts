f1c824bf526c4fff17800630fa29433e45965e58
RetiredHunter <belhassinehedi308@gmail.com>
2026-07-20 17:50:14 -0400

    #25 - Added deleteTask

--- Backend/src/controllers/task.controller.ts
+++ Backend/src/controllers/task.controller.ts
@@ -2,12 +2,10 @@ import type { Request, Response } from "express";
 import prisma from "../db/prisma";
 import { connect } from "node:http2";
 
-// const tasks: task[] = [];
+
 let nextId = 1;
 
 export async function createTask(req: Request, res: Response) {
-  // console.log('Raw request body:', JSON.stringify(req.body, null, 2));
-  // console.log('Content-Type:', req.headers['content-type']);
   const {
     title,
     note,
@@ -40,31 +38,36 @@ export async function createTask(req: Request, res: Response) {
       isPrivate,
     },
   });
-
   res.status(201).json(newTask);
-  // tasks.push(newTask);
 }
-// export function getAllTasks(req: Request, res: Response) {
-//   res.json(tasks);}
 
 export async function getAllTasks(req: Request, res: Response) {
+  const user_id = req.params.user_id as string
   const tasks = await prisma.task.findMany({
-    where: { user_id: "49c1c38b-cba4-4980-9b8d-03451eaf70b2" }
+    where: { user_id: user_id }
   })
   console.log(tasks)
   console.log(JSON.stringify(tasks))
-  res.status(200).json(tasks);
+
+  if (!tasks) return res.status(404).json({ error: "No tasks found." })
+  res.status(200).json(tasks)
 }
 
 export async function getTask(req: Request, res: Response) {
-  const task_id = req.params.task_id as string;
-
-  console.log('Raw request params:', JSON.stringify(req.params, null, 2));
-
-
+  // console.log('Raw request params:', JSON.stringify(req.params, null, 2));
+  const task_id = req.params.task_id as string
   const task = await prisma.task.findUnique({
     where: { task_id: task_id }
   })
   
+  if (!task) return res.status(404).json({ error: "Task not found." })
+  res.status(200).json(task)
+}
+
+export async function deleteTask(req: Request, res: Response){
+  const task_id = req.params.task_id as string
+  const task = await prisma.task.delete({
+    where: { task_id: task_id }
+  })
   res.status(200).json(task)
 }
