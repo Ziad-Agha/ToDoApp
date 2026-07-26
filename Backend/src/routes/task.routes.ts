06d3469f20a213c1d4a848c01566f9d97edd0a00
Ziad-Agha <123512724+Ziad-Agha@users.noreply.github.com>
2026-07-25 22:04:21 -0400

    #26 added route

--- Backend/src/routes/task.routes.ts
+++ Backend/src/routes/task.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createTask,  } from "../controllers/task.controller";
+import { createTask, updateTask } from "../controllers/task.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.use(authMiddleware);
 
-router.post("/createTask",createTask);
+router.post("/createTask", createTask);
+router.put("/updateTask/:task_id", updateTask);
 
 export default router;
