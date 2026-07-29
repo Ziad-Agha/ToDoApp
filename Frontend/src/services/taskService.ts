8adea8753362d91f094d13c264e5cbca0451e5ca
Ziad-Agha <123512724+Ziad-Agha@users.noreply.github.com>
2026-07-28 20:24:26 -0400

    #26 - created taskView component for updating tasks

--- Frontend/src/services/taskService.ts
+++ Frontend/src/services/taskService.ts
@@ -1,4 +1,4 @@
-import { apiFetch } from "../utils/api"
+import { apiFetch } from "../utils/api";
 
 interface Task {
   user_id: string;
@@ -14,32 +14,48 @@ interface Task {
   status: string;
   value: number;
 }
+interface TaskUpdate {
+title: string, note: string, isPrivate: boolean}
 
 // INACTIVE
 export async function getDailyTasks(day: Date): Promise<Task[]> {
-  const start = new Date(day); start.setHours(0, 0, 0, 0)
-  const end = new Date(day); end.setHours(23, 59, 59, 999)
+  const start = new Date(day);
+  start.setHours(0, 0, 0, 0);
+  const end = new Date(day);
+  end.setHours(23, 59, 59, 999);
 
-  const response = await apiFetch(`/tasks/getDailyTasks?start=${start.toISOString()}&end=${end.toISOString()}`,
-    { method: "GET" })
-  if (!response.ok) throw new Error(`Server error: ${response.status}`)
-  return response.json()
+  const response = await apiFetch(
+    `/tasks/getDailyTasks?start=${start.toISOString()}&end=${end.toISOString()}`,
+    { method: "GET" },
+  );
+  if (!response.ok) throw new Error(`Server error: ${response.status}`);
+  return response.json();
 }
 
 export async function getActiveTasks(): Promise<Task[]> {
-  const response = await apiFetch("/tasks/getActiveTasks",
-    { method: "GET" })
+  const response = await apiFetch("/tasks/getActiveTasks", { method: "GET" });
 
-  if (!response.ok)
-    throw new Error(`Server error: ${response.status}`)
+  if (!response.ok) throw new Error(`Server error: ${response.status}`);
 
-  return response.json() as Promise<Task[]>
+  return response.json() as Promise<Task[]>;
 }
 
 export function filterTasks(tasks: Task[]) {
-  const regulars = tasks.filter(task => task.frequency > 0)
-  const uniques = tasks.filter(task => task.frequency == 0 && task.status != "pending")
-  const pendings = tasks.filter(task => task.status === "pending")
+  const regulars = tasks.filter((task) => task.frequency > 0);
+  const uniques = tasks.filter(
+    (task) => task.frequency == 0 && task.status != "pending",
+  );
+  const pendings = tasks.filter((task) => task.status === "pending");
 
-  return { regulars, uniques, pendings }
+  return { regulars, uniques, pendings };
+}
+export async function updateRequest(task_id: string,{title, note, isPrivate}: TaskUpdate): Promise<Task> {
+  const response = await apiFetch(`/tasks/updateTask/${task_id}`, {
+    method: "PUT",
+    body: JSON.stringify({title, note, isPrivate})
+  });
+
+  if (!response.ok) throw new Error(`Server error: ${response.status}`);
+
+  return response.json() as Promise<Task>;
 }
