4f3ea572b2095bea5aa286170cc7ff21684887b5
RetiredHunter <belhassinehedi308@gmail.com>
2026-07-30 19:55:21 -0400

    #36 - Added date awareness: DateStore and DateNav

--- Frontend/src/assets/store.ts
+++ Frontend/src/assets/store.ts
@@ -18,3 +18,31 @@ export const useTaskFormStore = create<TaskFormStore>((set) => ({
     closeForm: () => set({ isFormOpen: false }),
 
 }))
+
+type DateStore = {
+    currentDate: Date
+    nextDay: () => void
+    prevDay: () => void
+}
+
+/*  On using next instead of currentDate
+
+    I creat a new object (next) instead of altering the existing currentDate, 
+    because Zustand detects change by comparing obejct references. 
+
+    Mutating an existing obj  => same obj ref  =>  doesn't trigger re-render
+    Newly created object      => new obj ref   =>  triggers a re-render
+*/
+export const useDateStore = create<DateStore>((set) => ({
+    currentDate: new Date(),
+    nextDay: () => set((date) => {
+        const next = new Date(date.currentDate)
+        next.setDate(next.getDate() + 1)
+        return { currentDate: next }
+    }),
+    prevDay: () => set((date) => {
+        const prev = new Date(date.currentDate)
+        prev.setDate(prev.getDate() - 1)
+        return { currentDate: prev }
+    })
+}))
