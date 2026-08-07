4f39371749b567415525ad6b2fcc7455aa2de717
Ziad-Agha <123512724+Ziad-Agha@users.noreply.github.com>
2026-08-07 16:31:57 -0400

    #52 in progess adding sub routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import LoginForm from "./Components/LoginForm";
 import RegisterForm from "./Components/RegisterForm";
-
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginForm />}></Route>
       <Route path="/register" element={<RegisterForm />}></Route>
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/" element={<LoginForm />}></Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/home" element={<HomePage />}>
+          {/* <Route index element={<Navigate to="daily" />} />
+          <Route path="daily" element={<DailyView />} />
+          <Route path="weekly" element={<WeeklyView />} /> */}
+        </Route>
+      </Route>
     </Routes>
   );
 }
